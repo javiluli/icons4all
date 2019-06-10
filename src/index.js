b7fc5ea2b63ec6e7d52fd0f2ddeab35c576b4772
javiluli <javidel96@hotmail.es>
2019-06-11 00:59:59 +0200

    Finished first version

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-// Import Icons website
-import "./static/content/svg/logo.svg";
-
-//---------------------------------//
-// Import CSS, Normalize and fonts.css
-import "./styles/style.scss";
-import "./static/css/font-Confortaa.css";
-import "./static/css/font-Montserrat.css";
-import "./static/css/normalize.css";
-//---------------------------------//
-
-//---------------------------------//
-// Import fonts.ttf
-import "./static/fonts/Comfortaa[wght].ttf";
-import "./static/fonts/Montserrat-Regular.ttf";
-import "./static/fonts/Montserrat-Medium.ttf";
-import "./static/fonts/Montserrat-SemiBold.ttf";
-//---------------------------------//
-import "./static/content/svg/battery-charging.svg";
-
-// Import Icons SVG for download
-
 // Import all filder from ./static/content/svg/
 const cache = {};
 function importAll(r) {
@@ -28,8 +6,21 @@ function importAll(r) {
 importAll(require.context("./static/content/svg/", true, /\.svg$/));
 //---------------------------------//
 
+// Import CSS, Normalize and fonts.css from ./styles
+import "./styles/style.scss";
+import "./static/css/font-Confortaa.css";
+import "./static/css/font-Montserrat.css";
+import "./static/css/normalize.css";
 //---------------------------------//
 
+// Import fonts.ttf from ./static/fonts
+import "./static/fonts/Comfortaa[wght].ttf";
+import "./static/fonts/Montserrat-Regular.ttf";
+import "./static/fonts/Montserrat-Medium.ttf";
+import "./static/fonts/Montserrat-SemiBold.ttf";
+//---------------------------------//
+
+// Import script ./static/js
 import "./static/js/show_menu";
 import "./static/js/array_SVG_library.js";
 import "./static/js/btn-back-top";
